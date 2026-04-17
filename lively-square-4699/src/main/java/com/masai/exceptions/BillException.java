2e373e7cc319562a6b07d85a8a09a445bf5de84d
varshith84 <vvarshith16@gmail.com>
2026-04-17 21:01:54 +0530

    first commit

---
+++ lively-square-4699/src/main/java/com/masai/exceptions/BillException.java
@@ -0,0 +1,20 @@
+package com.masai.exceptions;
+
+
+
+public class BillException extends Exception{
+	
+	
+
+	public BillException() {
+		// TODO Auto-generated constructor stub
+
+		
+		super();
+	}
+	
+	public BillException(String message) {
+		
+		super(message);
+	}
+}
